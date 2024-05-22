ec805f985d4d5f597cf23e75657836fd71b22b95
whydenyscry <gogool124512@gmail.com>
2024-05-22 16:42:15 +0300

    add odeEmbeddedGeneral_optimized.m

---
+++ odeEmbeddedGeneral_optimized.m
@@ -0,0 +1,27 @@
+function [t, xsol, ELE] = odeEmbeddedGeneral_optimized(c_vector, A_matrix, b_vector, b_hat_vector, odefun, tspan, tau, incond)
+
+s_stages = length(c_vector);
+m = length(incond);
+
+c_vector = reshape(c_vector, [s_stages 1]);
+b_vector = reshape(b_vector, [s_stages 1]);
+b_hat_vector = reshape(b_hat_vector, [s_stages 1]);
+d_vector = b_hat_vector - b_vector;
+incond = reshape(incond, [m 1]);
+
+t = (tspan(1) : tau : tspan(2))';
+xsol = zeros(length(incond), length(t));
+xsol(:, 1) = incond(:);
+K_matrix = zeros(m, s_stages);
+ELE = zeros(length(t), 1);
+
+for n = 1:length(t)-1
+    K_matrix(:, 1) = odefun(t(n), xsol(:, n));   
+        for i = 2:s_stages
+            K_matrix(:, i) = odefun(t(n) + tau * c_vector(i), xsol(:, n) + tau * K_matrix(:, 1:i-1) * A_matrix(i, 1:i-1)');
+        end
+    xsol(:, n+1) = xsol(:, n) + tau * K_matrix * b_vector;
+    ELE(n+1) = norm(tau * K_matrix * d_vector, "inf");
+end
+xsol = xsol';
+end
