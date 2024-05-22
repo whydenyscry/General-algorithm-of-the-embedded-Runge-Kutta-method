2a05f96a88d0e979f87f1cccf8bca2af012d4098
whydenyscry <gogool124512@gmail.com>
2024-05-22 16:50:31 +0300

    rename some arguments

--- odeFSALEmbeddedGeneral.m
+++ odeFSALEmbeddedGeneral.m
@@ -1,4 +1,4 @@
-function [t, xsol, ELE] = odeFSALEmbeddedGeneral(c_vector, A_matrix, b_vector, b_hat_vector, fun, timeint, tau, incond)
+function [t, xsol, ELE] = odeFSALEmbeddedGeneral(c_vector, A_matrix, b_vector, b_hat_vector, odefun, tspan, tau, incond)
 
 s_stages = length(c_vector);
 m = length(incond);
@@ -10,11 +10,11 @@ d_vector = b_hat_vector - b_vector;
 
 incond = reshape(incond, [m 1]);
 
-t = (timeint(1) : tau : timeint(2))';
+t = (tspan(1) : tau : tspan(2))';
 xsol = zeros(length(incond), length(t));
 xsol(:, 1) = incond(:);
 ELE = zeros(length(t), 1);
-fsal = fun(t(1), xsol(:, 1));
+fsal = odefun(t(1), xsol(:, 1));
 
 for n = 1:length(t)-1
     t_n = t(n);
@@ -28,7 +28,7 @@ for n = 1:length(t)-1
             t_arg = t_n + tau_n * c_vector(i);
             a_vector = A_matrix(i, 1:i-1)';
             x_arg = x_n + tau_n * K_matrix(:, 1:i-1) * a_vector;
-            K_matrix(:, i) = fun(t_arg, x_arg);
+            K_matrix(:, i) = odefun(t_arg, x_arg);
         end
     
     xsol(:, n+1) = x_n + tau_n * K_matrix * b_vector;
